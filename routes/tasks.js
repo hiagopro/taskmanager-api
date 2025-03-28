047e4c6c7569492582df0eb6b76df629b5b1cd26
hiagopro <areazica@gmail.com>
2025-03-28 09:30:21 -0300

    ok

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("dotenv").config();
 const authenticateToken = require("../middlewares/authenthicationToken");
 const getConnection = require("../connect");
 const jwt = require("jsonwebtoken");
