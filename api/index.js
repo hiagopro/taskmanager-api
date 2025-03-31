2d4a2493a7efc4e4092d3e1767d90a32aee61185
hiagopro <areazica@gmail.com>
2025-03-31 10:40:04 -0300

    ok

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 const app = require("../app");
 
-app.listen(() => console.log(`Server running on port:${5000} `));
+app.listen(process.env.PORT, () =>
+  console.log(`Server running on port:${process.env.PORT} `)
+);
 
 module.exports = app;
