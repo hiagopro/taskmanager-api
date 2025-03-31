894de5ac9392bbec8cf078817a46a0b50edfb74f
hiagopro <areazica@gmail.com>
2025-03-31 11:34:28 -0300

    ok

--- middlewares/authenthicationToken.js
+++ middlewares/authenthicationToken.js
@@ -1,17 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-const secret = 'T2NzMFVhVzM4ZW9VdVR5Y3g0a0pXbTFRbGRaZG5mYzZB'
+const secret = process.env.secret;
 function authenticateToken(req, res, next) {
   const token = req.headers["token"];
-    
+
   if (!token) return res.status(403).send("Token is required");
 
   jwt.verify(token, secret, (err, decoded) => {
     if (err) {
-        if (err.name === 'TokenExpiredError') {
-            return res.status(401).json({ message: 'Token has expired' });
-          }
-          return res.status(403).json({ message: 'Invalid token' });
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token has expired" });
+      }
+      return res.status(403).json({ message: "Invalid token" });
     }
 
     req.user = decoded;
