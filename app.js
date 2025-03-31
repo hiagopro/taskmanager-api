28fef983efa9cab5b57cd89ce8584baff922f891
hiagopro <areazica@gmail.com>
2025-03-31 11:20:12 -0300

    ok

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ require("dotenv").config({ path: ".env" });
 const express = require("express");
 const app = express();
 const cors = require("cors");
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.debug({ origin, whitelist }, "inicio da origin");
+
+      console.debug("passou no if");
+      callback(null, true); // Permite requisições same-origin
+    },
+  })
+);
 const routerTasks = require("./routes/tasks.js");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,28 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      console.debug({ origin, whitelist }, "inicio da origin");
-      if (origin === undefined) {
-        console.debug("passou no if");
-        return callback(null, { origin: true }); // Permite requisições same-origin
-      }
 
-      if (whitelist.indexOf(origin) !== -1) {
-        return callback(null, true); // Permite a origem se estiver na whitelist
-      }
-
-      const errorMessage = `The CORS policy for this site does not allow access from '${origin}'. Allowed origins: [${whitelist
-        .map((acceptedOrigin) => `'${acceptedOrigin}'`)
-        .join(", ")}]`;
-      console.debug({ origin, whitelist, errorMessage }, "final da origin");
-
-      return callback(new Error(errorMessage), false);
-    },
-  })
-);
 app.use("/", routerTasks);
 
 module.exports = app;
