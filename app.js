5f340547b5fe68db4c7a93a1f07da6e659a99d3e
hiagopro <areazica@gmail.com>
2025-03-31 11:31:51 -0300

    ok

--- app.js
+++ app.js
@@ -13,27 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      console.debug({ origin, whitelist });
-      if (!origin) {
-        return callback(null, true); // Permite requisições same-origin
-      }
-
-      if (whitelist.indexOf(origin) !== -1) {
-        return callback(null, true); // Permite a origem se estiver na whitelist
-      }
-
-      const errorMessage = `The CORS policy for this site does not allow access from '${origin}'. Allowed origins: [${whitelist
-        .map((acceptedOrigin) => `'${acceptedOrigin}'`)
-        .join(", ")}]`;
-
-      console.warn(errorMessage);
-      return callback(new Error(errorMessage), false);
-    },
-  })
-);
+app.use(cors());
 app.use("/", routerTasks);
 
 module.exports = app;
