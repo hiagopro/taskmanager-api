484d9a00460e252d8003dfc6632958f902b53b2d
hiagopro <areazica@gmail.com>
2025-03-31 11:29:41 -0300

    ok

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ require("dotenv").config({ path: ".env" });
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.use(cors());
 const routerTasks = require("./routes/tasks.js");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,7 +13,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      console.debug({ origin, whitelist });
+      if (!origin) {
+        return callback(null, true); // Permite requisições same-origin
+      }
 
+      if (whitelist.indexOf(origin) !== -1) {
+        return callback(null, true); // Permite a origem se estiver na whitelist
+      }
+
+      const errorMessage = `The CORS policy for this site does not allow access from '${origin}'. Allowed origins: [${whitelist
+        .map((acceptedOrigin) => `'${acceptedOrigin}'`)
+        .join(", ")}]`;
+
+      console.warn(errorMessage);
+      return callback(new Error(errorMessage), false);
+    },
+  })
+);
 app.use("/", routerTasks);
 
 module.exports = app;
